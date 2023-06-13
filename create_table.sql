54749628dc1fb0a06f205e3dedf0f5ba01cbbf21
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-06-13 22:21:46 +0300

    add basic create table

---
+++ create_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE cars (
+    model VARCHAR (100),
+    year_of_production INT,
+    price numeric( 8,2),
+    is_used boolean,
+    created_at date
+
+);
+
+
+DROP TABLE cars;
+
+
---
+++ db.sql
@@ -0,0 +1,15 @@
+/*
+CREATE DATABASE training_db;
+CREATE DATABASE keks;
+
+DROP DATABASE training_db;
+DROP DATABASE keks;
+
+*/
+
+CREATE TABLE my_first_table(
+    first_column text,
+    second_column text
+);
+
+DROP TABLE my_first_table;
