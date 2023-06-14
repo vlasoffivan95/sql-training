a19de2b3063e883ab6dfc8743f4b3dddb97fb0a8
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-06-14 22:26:42 +0300

    unique for table

--- insert.sql
+++ insert.sql
@@ -3,7 +3,7 @@ VALUES ('Best model', 2022, 19999.99, false, '2022-12-26');
 
 
 INSERT INTO cars (model, price, year_of_production, vin_number, is_used, created_at)
-VALUES('new BMW', 75000, 2024, 234, true, '2022-12-27'),
+VALUES('new BMW', 75000, 2024, 2394, true, '2022-12-27'),
 ('new Ford', -25000, 2024, 123, true, '2022-12-27'),
 ('', 75000, 2024, 234, true, '2029-12-27');
 
